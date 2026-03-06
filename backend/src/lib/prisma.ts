d1b160b972f74ecc36cbca13ebc5dd404cd5cc5a
Ubuntu <ubuntu@vps-e87bcb64.vps.ovh.net>
2026-03-06 20:29:08 +0000

    fix: configure prisma adapter correctly

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,6 +2,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "../config";
 import { PrismaClient } from "../generated/prisma/client";
 
-const adapter = new PrismaPg(env.DATABASE_URL);
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
 
 export const prisma = new PrismaClient({ adapter });
